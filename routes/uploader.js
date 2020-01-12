6b4d3be15ee3cb535557bb469b8a57bde15b5796
bigxigua <1336945839@qq.com>
2020-01-12 13:23:12 +0800

    feat: 修改upload file return url

--- routes/uploader.js
+++ routes/uploader.js
@@ -19,7 +19,7 @@ router.post('/upload/image', upload.single('file'), async (ctx, next) => {
         const filePath = `${path.join(__dirname, '../upload/file/images/')}${filename}.${suffix}`;
         await fs.writeFileSync(filePath, file.buffer);
         ctx.body = serializReuslt('SUCCESS', {
-            path: `${hostname}:${port}/file/images/${filename}.${suffix}`
+            path: `${hostname}/file/images/${filename}.${suffix}`
         });
     } catch (error) {
         console.log('-------上传文件出错--------', error);
