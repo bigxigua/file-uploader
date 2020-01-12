2ac041285dcdb5fdc3fcfcfc8e8e519548201b2c
bigxigua <1336945839@qq.com>
2020-01-12 12:08:38 +0800

    feat: 新增file 上传接口

--- config/server-config.js
+++ config/server-config.js
@@ -1,6 +1,5 @@
 const NODE_ENV = process.env.env;
 const domain = NODE_ENV === 'development' ? '127.0.0.1' : 'www.bigxigua.net';
-
 exports.cookieConfig = {
     domain,
     signed: true,
@@ -12,4 +11,4 @@ exports.cookieConfig = {
 
 exports.hostname = NODE_ENV === 'development' ? 'http://127.0.0.1' : 'https://bigxigua.net';
 
-exports.port = 8080;
+exports.port = 3000;
--- routes/uploader.js
+++ routes/uploader.js
@@ -6,42 +6,28 @@ const { hostname, port } = require('../config/server-config');
 const multer = require('@koa/multer');
 const execa = require('execa');
 
-const SERVER_PATH = `${hostname}:${port}/img`;
 const upload = multer();
-// const UPLOAD_IMG_PATH = path.join(process.cwd(), './public/upload/');
 
-function asyncFileWriteStreamClose(stream) {
-    return new Promise((resolve) => {
-        stream.on('close', function () {
-            resolve(true);
-        });
-        stream.on('error', function (e) {
-            resolve(false);
-            console.log(e, '-------error-------');
-        });
-    });
-};
+// TODO 这两个接口均未鉴权。需要加权限。
 
-router.post('/upload/image', async (ctx, next) => {
-    const { files, body } = ctx.request;
+// 上传图片
+router.post('/upload/image', upload.single('file'), async (ctx, next) => {
+    const { file } = ctx.request;
     try {
-        const { fileId } = body;
-        const { file } = files;
-        const fileReaderStream = fs.createReadStream(file.path);
-        const filePath = path.join(__dirname, '../upload/') + `/${file.name}`;
-        const fileWriteStream = fs.createWriteStream(filePath);
-        fileReaderStream.pipe(fileWriteStream);
-        const success = await asyncFileWriteStreamClose(fileWriteStream, file.name);
-        if (success) {
-            ctx.body = serializReuslt('SUCCESS', { path: `${SERVER_PATH}/${file.name}`, fileId });
-        } else {
-            ctx.body = serializReuslt('SYSTEM_INNER_ERROR');
-        }
+        const filename = Date.now().toString();
+        const suffix = file.originalname.split('.').pop() || 'jpg';
+        const filePath = `${path.join(__dirname, '../upload/file/images/')}${filename}.${suffix}`;
+        await fs.writeFileSync(filePath, file.buffer);
+        ctx.body = serializReuslt('SUCCESS', {
+            path: `${hostname}:${port}/file/images/${filename}.${suffix}`
+        });
     } catch (error) {
         console.log('-------上传文件出错--------', error);
         ctx.body = serializReuslt('SYSTEM_INNER_ERROR', error);
     }
 });
+
+// 前端部署发布
 router.post('/upload/file', upload.single('file'), async (ctx, next) => {
     const { file } = ctx.request;
     try {
