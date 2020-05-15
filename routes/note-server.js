c41772afceb7e8f841939e41bc7687c68e0af286
bigxigua <1336945839@qq.com>
2020-05-15 21:27:30 +0800

    test: test note-server

--- routes/note-server.js
+++ routes/note-server.js
@@ -1,5 +1,6 @@
 const execa = require('execa');
 const path = require('path');
+const router = require('koa-router')();
 
 // 用来部署发布 note-server服务
 // 1. 到server服务器拉取note-server最新代码(操作之前查看diff和git提交记录)
@@ -10,4 +11,5 @@ const path = require('path');
 // 上传图片
 router.post('/upload/note-server', async (ctx, next) => {
   console.log('pwd:', __dirname);
+  console.log('process.cwd():', process.cwd());
 });
